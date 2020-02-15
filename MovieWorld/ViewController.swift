85461161ce8529aecaec798ec7ef54d2e91588bf
krokonox <kuricyna20@gmail.com>
2020-02-15 17:21:38 +0300

    Initial Commit

---
+++ MovieWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieWorld
+//
+//  Created by Admin on 15/02/2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
